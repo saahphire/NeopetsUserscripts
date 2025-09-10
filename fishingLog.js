2ef30b3738ccd199c4a228fa79b7efe3978e3ca8
Saah <saahloss@gmail.com>
2025-09-10 10:41:59 -0300

    Fix extra "> in action select

--- fishingLog.js
+++ fishingLog.js
@@ -618,7 +618,7 @@ const html = `
             <label for="fishing-log-manual-pet">Pet</label>
             <input type="text" id="fishing-log-manual-pet">
             ${activateSep2025Challenge ? '<label for="fishing-log-manual-action">Action</label>' : ''}
-            ${activateSep2025Challenge ? '<select id="fishing-log-manual-action"><option value="' + fishingPoints.map(([actionName, _], i) => i + '">' + actionName).join('</option>\n<option value="') + '"></option></select>' : ''}
+            ${activateSep2025Challenge ? '<select id="fishing-log-manual-action"><option value="' + fishingPoints.map(([actionName, _], i) => i + '">' + actionName).join('</option>\n<option value="') + '</option></select>' : ''}
             <label for="fishing-log-manual-category">Category</label>
             <input type="text" id="fishing-log-manual-category">
             <label for="fishing-log-manual-details">Details</label>
