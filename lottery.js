f90ea8df9a97434a8f116e66adf5542cc2f37963
Saah <saahloss@gmail.com>
2025-10-15 20:42:52 -0300

    Now it checks for repeated numbers

--- lottery.js
+++ lottery.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         Neopets: Lottery Numbers Generator (Works Oct 2025)
+// @name         Neopets: Lottery Numbers Generator (Real) (Works) (I promise)
 // @namespace    https://github.com/saahphire/NeopetsUserscripts
-// @version      0.1.0
+// @version      1.0.0
 // @description  Generates not-so-random numbers for you to play in the lottery, making sure you repeat as little numbers as possible to increase your chances of winning. And it actually works! Unlike 90% of the lottery userscripts!
 // @author       saahphire
 // @homepageURL  https://github.com/saahphire/NeopetsUserscripts
 // @homepage     https://github.com/saahphire/NeopetsUserscripts
-// @downloadURL  https://github.com/saahphire/NeopetsUserscripts/blob/testing/lottery.js
-// @updateURL    https://github.com/saahphire/NeopetsUserscripts/blob/testing/lottery.js
+// @downloadURL  https://github.com/saahphire/NeopetsUserscripts/blob/main/lottery.js
+// @updateURL    https://github.com/saahphire/NeopetsUserscripts/blob/main/lottery.js
 // @match        *://*.neopets.com/games/lottery.phtml
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=neopets.com
 // @license      The Unlicense
@@ -32,17 +32,33 @@
 •:•.•:•.•:•:•:•:•:•:•:••:•.•:•.•:•:•:•:•:•:•:•:•.•:•.•:•:•:•:•:•:•:••:•.•:•.•:•.•:•:•:•:•:•:•:•:•.•:•:•.•:•.••:•.•:•.••:
 */
 
-// Add your lucky numbers here!
-// If you'd like to get rid of the lucky button instead, change "true" to "false"
+// Add your lucky numbers here! If you'd like to get rid of the lucky button instead, change "true" to "false"
 const enableLuckyButton = true;
 const luckyNumbers = [2, 7, 11, 20, 25, 27];
 
 const inputNames = ["one", "two", "three", "four", "five", "six"];
 
-const getNewNumber = (numberFrequency, currentNumbers) => {
-    const randomNumber = Math.ceil(Math.random() * 30);
-    if(numberFrequency[randomNumber] > 3 || currentNumbers.includes(randomNumber)) return getNewNumber(numberFrequency, currentNumbers);
-    return randomNumber;
+const shuffleArray = (array) => {
+    // In my humble opinion if Durstenfeld had credited Fisher-Yates, it should be called a Durstenfeld shuffle, but karma got him
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+const isValidArray = (array) => {
+    for(let i = 5; i < array.length; i++) if(array.slice(Math.max(0, i - 6), i).includes(array[i])) return false;
+    const subArrays = [];
+    for(let i = 0; i < array.length; i += 6) subArrays.push(array.slice(i, i + 6));
+    for(let i = 0; i < subArrays.length; i++) if(subArrays.slice(0, i).includes(subArrays[i])) return false;
+    return true;
+}
+
+const getShuffledArray = (array, numberOfTries = 0) => {
+    if(isValidArray(array)) return array;
+    if(numberOfTries > 250) throw new Error(`Maximum shuffling tries reached. Tried to shuffle the following array: [${array.join(", ")}]`);
+    return getShuffledArray(shuffleArray(array), numberOfTries + 1);
 }
 
 const addLuckyButton = (form) => {
@@ -85,19 +101,44 @@ const changeHTML = (canBuy) => {
     }
 }
 
+const pickRandomIndex = (array, condition, attempts = 0) => {
+    if(attempts === 250) throw new Error(`Couldn't find any indices satifying the condition! Array: [${array.join(", ")}]`);
+    const picked = Math.floor(Math.random() * array.length);
+    return (array[picked] >= condition) ? picked : pickRandomIndex(array, condition, attempts + 1);
+}
+
+const getRandomTicketNumbers = (currentNumbers) => {
+    if(currentNumbers.length === 6) return currentNumbers;
+    const newNumber = Math.ceil(Math.random() * 30);
+    return getRandomTicketNumbers(currentNumbers.includes(newNumber) ? currentNumbers : currentNumbers.concat([newNumber]));
+}
+
 const fillLotteryNumbers = () => {
-    const previousTicketNumbers = [...document.querySelector(".content p:nth-of-type(3)").childNodes].filter((e, i) => i % 4 === 2).flatMap(txt => txt.textContent.match(/\d+/g));
-    if(previousTicketNumbers.length > 119) return false;
-    const frequency = {};
-    previousTicketNumbers.forEach(number => {
-        frequency[number] = (frequency[number] ?? 0) + 1
+    const frequencies = (new Array(30)).fill(4);
+    const previousTicketNumbers = [...document.querySelector(".content p:nth-of-type(3)").childNodes].filter((e, i) => i % 4 === 2).flatMap(txt => {
+        const number = txt.textContent.match(/\d+/g);
+        frequencies[number - 1]--;
+        return number;
     });
-    const currentTicketNumbers = [];
-    for(let i = 0; i < 6; i++) {
-        const newNumber = getNewNumber(frequency, currentTicketNumbers);
-        frequency[newNumber] = (frequency[newNumber] ?? 0) + 1;
-        currentTicketNumbers.push(newNumber);
-        document.querySelector(`input[name="${inputNames[i]}"]`).value = newNumber;
+    const nextNumberQuantity = 120 - previousTicketNumbers.length;
+    if(nextNumberQuantity <= 0) return false;
+    const nextTicketNumbers = [];
+    frequencies.forEach((frequency, index) => {
+        if(frequency > nextNumberQuantity / 6) return;
+        nextTicketNumbers.concat((new Array(frequency).fill(index + 1)));
+    });
+    let ticketNumbers = [];
+    try {
+        for(let i = nextTicketNumbers.length + previousTicketNumbers.length; i < 120; i++) nextTicketNumbers.push(pickRandomIndex(frequencies, 0));
+        console.log(nextTicketNumbers);
+        ticketNumbers = getShuffledArray(nextTicketNumbers);
+    } catch(e) {
+        document.querySelector(".content p:nth-of-type(2)").insertAdjacentHTML("beforeBegin", `<p style="color:darkred">Something went wrong! You can buy non-optimized numbers like usual or refresh the page. If you see this message again after refreshing the page, please <a href="https://www.neopets.com/neomessages.phtml?type=send&recipient=saahphire">neomail saahphire</a> with your current lottery numbers and the following text:</p><p style="color:red;">${e}</p>`);
+        ticketNumbers = getRandomTicketNumbers([]);
+    } finally {
+        inputNames.forEach((name, index) => {
+            document.querySelector(`input[name="${name}"]`).value = ticketNumbers[index];
+        });
     }
     return true;
 }
