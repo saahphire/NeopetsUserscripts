1d9351a4971c5cda545aec044e42078dcef43a2a
Saah <saahloss@gmail.com>
2026-01-19 21:29:05 -0300

    Create noReloadOnRandomEvent.js

---
+++ noReloadOnRandomEvent.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Neopets: No Reload on Random Event
+// @namespace    https://github.com/saahphire/NeopetsUserscripts
+// @version      1.0.0
+// @description  Prevents you from exiting a page (with a pop-up) if a random event has been triggered in it.
+// @author       saahphire
+// @homepageURL  https://github.com/saahphire/NeopetsUserscripts
+// @homepage     https://github.com/saahphire/NeopetsUserscripts
+// @downloadURL  https://github.com/saahphire/NeopetsUserscripts/blob/main/noReloadOnRandomEvent.js
+// @updateURL    https://github.com/saahphire/NeopetsUserscripts/blob/main/noReloadOnRandomEvent.js
+// @match        *://*.neopets.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=neopets.com
+// @license      Unlicense
+// ==/UserScript==
+
+/*
+•:•.•:•.•:•:•:•:•:•:•:••:•.•:•.•:•:•:•:•:•:•:•:•.•:•.•:•:•:•:•:•:•:••:•.•:•.•:•.•:•:•:•:•:•:•:•:•.•:•:•.•:•.••:•.•:•.••:
+........................................................................................................................
+☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦
+    This script jumpscares you with the "Are you sure you want to exit?" pop-up if you're trying to exit a page with a
+    random event in it. Useful if you're testing something and not for much else.
+
+    ✦ ⌇ saahphire
+☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦
+........................................................................................................................
+•:•.•:•.•:•:•:•:•:•:•:••:•.•:•.•:•:•:•:•:•:•:•:•.•:•.•:•:•:•:•:•:•:••:•.•:•.•:•.•:•:•:•:•:•:•:•:•.•:•:•.•:•.••:•.•:•.••:
+*/
+
+(function() {
+    'use strict';
+    if(document.querySelector(".randomEvent")) 
+        window.addEventListener("beforeunload", e => e.preventDefault());
+})();
