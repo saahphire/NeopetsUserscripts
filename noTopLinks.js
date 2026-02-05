e8a685bf07fbfd09e167b3cac233995882cba3c8
Saah <saahloss@gmail.com>
2026-02-05 12:22:44 -0300

    Create noTopLinks.js

---
+++ noTopLinks.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Neopets: No Top Links
+// @namespace    https://github.com/saahphire/NeopetsUserscripts
+// @version      1.0.0
+// @description  Removes all top links from the old layout and adds SDB to the Shops dropdown menu
+// @author       saahphire
+// @homepageURL  https://github.com/saahphire/NeopetsUserscripts
+// @homepage     https://github.com/saahphire/NeopetsUserscripts
+// @downloadURL  https://github.com/saahphire/NeopetsUserscripts/blob/main/noTopLinks.js
+// @updateURL    https://github.com/saahphire/NeopetsUserscripts/blob/main/noTopLinks.js
+// @match        *://*.neopets.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=neopets.com
+// @license      Unlicense
+// ==/UserScript==
+
+/*
+•:•.•:•.•:•:•:•:•:•:•:••:•.•:•.•:•:•:•:•:•:•:•:•.•:•.•:•:•:•:•:•:•:••:•.•:•.•:•.•:•:•:•:•:•:•:•:•.•:•:•.•:•.••:•.•:•.••:
+........................................................................................................................
+☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦
+    This script removes the following links from the top of old layout pages:
+        - Redundant: Shop Wizard, Auctions, Trading Post, Inventory, Your Shop, Battledome, Guild Headquarters
+        - To broken areas: Notice Board, NeoHomes, Storage Shed
+        - That nobody cares about: Neopia Central, Bazaar, Plaza, Marketplace
+
+    ✦ ⌇ saahphire
+☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦
+........................................................................................................................
+•:•.•:•.•:•:•:•:•:•:•:••:•.•:•.•:•:•:•:•:•:•:•:•.•:•.•:•:•:•:•:•:•:••:•.•:•.•:•.•:•:•:•:•:•:•:•:•.•:•:•.•:•.••:•.•:•.••:
+*/
+
+const linksStillThere = [
+    ['https://neopets.com/closet.phtml', 'Closet'],
+    ['https://neopets.com/safetydeposit.phtml', 'Safety Deposit Box']
+];
+
+(function() {
+    'use strict';
+    document.querySelectorAll('.medText:has(a[href="/neohome/"]), .medText:has(a[href="/neohome/"]) + br').forEach(e => e.remove());
+    document.querySelector('.dropdown li:has(a[href="/bank.phtml"])')?.insertAdjacentHTML('afterend', '<li><a href="https://www.neopets.com/safetydeposit.phtml">» Safety Deposit Box</a></li>');
+})();
