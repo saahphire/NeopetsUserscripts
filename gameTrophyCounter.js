afedb08368c7b9235786cc669cdbeac14b2bfb30
Saah <saahloss@gmail.com>
2026-03-02 05:59:20 -0300

    Create gameTrophyCounter.js

---
+++ gameTrophyCounter.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Neopets: Game Trophy Counter
+// @namespace    https://github.com/saahphire/NeopetsUserscripts
+// @version      1.0.0
+// @description  Adds a counter to every userlookup's game trophies section title
+// @author       saahphire
+// @homepageURL  https://github.com/saahphire/NeopetsUserscripts
+// @homepage     https://github.com/saahphire/NeopetsUserscripts
+// @downloadURL  https://github.com/saahphire/NeopetsUserscripts/blob/main/gameTrophyCounter.js
+// @updateURL    https://github.com/saahphire/NeopetsUserscripts/blob/main/gameTrophyCounter.js
+// @match        *://*.neopets.com/userlookup.phtml*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=neopets.com
+// @license      Unlicense
+// @grant        GM.setValue
+// @grant        GM.getValue
+// ==/UserScript==
+
+/*
+•:•.•:•.•:•:•:•:•:•:•:••:•.•:•.•:•:•:•:•:•:•:•:•.•:•.•:•:•:•:•:•:•:••:•.•:•.•:•.•:•:•:•:•:•:•:•:•.•:•:•.•:•.••:•.•:•.••:
+........................................................................................................................
+☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦
+    This script adds a number to the title of any lookup's game trophies section, counting how many trophies they have.
+    It works regardless of layout, unless someone hides their "- Game Trophies -" title, in which case they'll also
+    hide the counter.
+
+    ✦ ⌇ saahphire
+☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦
+........................................................................................................................
+•:•.•:•.•:•:•:•:•:•:•:••:•.•:•.•:•:•:•:•:•:•:•:•.•:•.•:•:•:•:•:•:•:••:•.•:•.•:•.•:•:•:•:•:•:•:•:•.•:•:•.•:•.••:•.•:•.••:
+*/
+
+(function() {
+    'use strict';
+    const count = document.querySelectorAll('div + hr[size="1"] + div:has(b) ~ div:not(:has( ~ div)) .trophy_cell').length;
+    const title = document.querySelector('div + hr[size="1"] + div:not(:has(~ div ~ div)) b');
+    if(title) title.textContent = title.textContent.replace(' -', ` (${count}) -`);
+})();
