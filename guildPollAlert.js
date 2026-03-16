d3f744b6fc8c7ac881d99e03c43f40866cacc83e
Saah <saahloss@gmail.com>
2026-03-16 20:41:10 -0300

    Fix @match only matching homepage

--- guildPollAlert.js
+++ guildPollAlert.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Neopets: Guild Poll Alert
 // @namespace    https://github.com/saahphire/NeopetsUserscripts
-// @version      1.0.0
+// @version      1.0.1
 // @description  Sends a native browser notification whenever you load a page in your guild and there's a new poll in it
 // @author       saahphire
 // @homepageURL  https://github.com/saahphire/NeopetsUserscripts
 // @homepage     https://github.com/saahphire/NeopetsUserscripts
 // @downloadURL  https://github.com/saahphire/NeopetsUserscripts/blob/main/guildPollAlert.js
 // @updateURL    https://github.com/saahphire/NeopetsUserscripts/blob/main/guildPollAlert.js
-// @match        *://*.neopets.com/guilds/guild.phtml?id=*
+// @match        *://*.neopets.com/guilds/guild*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=neopets.com
 // @license      Unlicense
 // @grant        GM.setValue
@@ -37,20 +37,18 @@ const isArrayEqual = (firstArray, secondArray) => {
     return true;
 }
 
-const getCurrentPoll = () => {
-    const desc = document.querySelector('tr:has(.content) + tr font[size="1"]')?.textContent;
-    const options = [...document.querySelectorAll('td[valign="middle"] font[size="1"]')].map(font => font.textContent);
-    return {desc, options};
-}
+const getCurrentPollOptions = () => [...document.querySelectorAll('td[valign="middle"] font[size="1"]')].map(font => font.textContent);
 
 (async function() {
     'use strict';
-    const currentPoll = getCurrentPoll();
+    const desc = document.querySelector('tr:has(.content) + tr font[size="1"]')?.textContent;
+    if(!desc) return;
+    const options = getCurrentPollOptions();
     const oldPoll = await GM.getValue('poll', {options: []});
-    if(currentPoll.desc !== oldPoll.desc || !isArrayEqual(currentPoll.options, oldPoll.options)) {
-        GM.setValue('poll', currentPoll);
+    if(desc !== oldPoll.desc || !isArrayEqual(options, oldPoll.options)) {
+        GM.setValue('poll', {desc, options});
         GM.notification({
-            text: currentPoll.desc, 
+            text: desc, 
             title: 'New Guild Poll!', 
             image: document.querySelector('img[width="100"][height="100"]').src
         });
