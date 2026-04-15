7d99c317c18ec30b35fa2b123311f893ce935bb9
Saah <saahloss@gmail.com>
2026-04-15 18:20:30 -0300

    Update for new Quick Stock

--- storeAll.js
+++ storeAll.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Neopets: Store All
 // @namespace    https://github.com/saahphire/NeopetsUserscripts
-// @version      1.1.0
-// @description  Selects closet (for wearables) or deposit for every item in your quick stock. Focuses on the submit button so you only have to press enter.
+// @version      1.2.0
+// @description  Selects closet (for wearables) or deposit for every item in your quick stock. Updated for new Quick Stock.
 // @author       saahphire
 // @homepageURL  https://github.com/saahphire/NeopetsUserscripts
 // @homepage     https://github.com/saahphire/NeopetsUserscripts
@@ -19,15 +19,13 @@
 ........................................................................................................................
 ☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦ ⠂⠄⠄⠂⠁⠁⠂⠄⠂⠄⠄⠂☆ ⠂⠄⠄⠂⠁⠁⠂⠄⠄⠂✦
     This script does the following:
-    - Selects "Discard" for every item that you consider too cheap
+    - Selects "Discard" for every item that you consider too cheap*
     - Selects "Closet" for every wearable item in your quickstock
+    - Selects "Shed" for every item that can go to your NeoHome shed
+    - Selects "Chamber" for every item that can go to your Chamber
     - Selects "Deposit" for all other items
-    - Focuses the submit button so all you have to do is press Enter
 
-    It doesn't conflict with my other script, Highlight Wearables in Quick Stock. As always, click twice on a radio
-    button to cancel the item's placement.
-
-    Discard only works if you also have my Quick Stock Pricer script installed (choose one source):
+    * Discard only works if you also have my Quick Stock Pricer script installed (choose one source):
     https://github.com/saahphire/NeopetsUserscripts/blob/main/quickStockPricer.js
     https://greasyfork.org/en/scripts/568260-neopets-quick-stock-pricer
 
@@ -51,36 +49,31 @@ const priceDiscardCutoff = 25;
 The order in which operations should be decided. If discard is before closet, an item with low
 value will be discarded even if it's wearable. If closet is before discard, an item with low
 value will be sent to your Closet instead of discarded if it's wearable. Shed and closet don't
-affect each other. All operations after SDB will be ignored, because every item can go to the
-SDB and no item will be left for the other operations. You may remove words from the list to
-disable them. CASE SENSITIVE. Must be in lowercase!
+affect each other. All operations after Deposit will be ignored, because every item can go to
+the deposit and no item will be left for the other operations. You may remove words from the
+list to disable them. CASE SENSITIVE. Must be in lowercase! You can replace 'discard' with
+'donate' if you want. They'll work the same way regarding price cutoff.
 •:•.•:•.•:•:•:•:•:•:•:••:•.•:•.•:•:•:•:•:•:•:•:•.•:•.•:•:•:•:•:•:•:••:•.•:•.•:•.•:•:•:•:•:•:•:•
 */
-const order = ['discard', 'closet', 'shed', 'sdb'];
+const order = ['discard', 'closet', 'shed', 'chamber', 'deposit'];
+
+const fire = (item, inputName) => {
+    const input = item.querySelector(`input[value="${inputName}"]`);
+    if(!input) return false;
+    const event = new Event('change', { bubbles: true });
+    input.checked = true;
+    input.dispatchEvent(event);
+    return true;
+}
 
 const discard = (item) => {
     const price = item.querySelector('[data-price]')?.dataset.price;
-    if(!price || parseInt(price) > priceDiscardCutoff) return false;
-    return item.querySelector('input[value="discard"]').checked = true;
+    return price && parseInt(price) <= priceDiscardCutoff;
 }
 
-const store = (item, storageName) => {
-    const radio = item.querySelector(`input[value="${storageName}"]`);
-    return radio && (radio.checked = true);
-}
+const select = (item, operation) => ((operation === 'discard' || operation === 'donate') && !discard(item)) ? false : fire(item, operation);
 
-const closet = (item) => store(item, 'closet');
-
-const shed = (item) => store(item, 'storage_shed');
-
-const sdb = (item) => store(item, 'deposit');
-
-const operations = { discard, closet, shed, sdb };
-
-const sort = (items) => {
-    items.forEach(item => order.find(operation => operations[operation](item)));
-    document.querySelector('[name="quickstock"] input[type="submit"]').focus();
-}
+const sort = (items) => items.forEach(item => order.find(operation => select(item, operation)));
 
 const watch = (items) => {
     const observer = new MutationObserver(() => {
