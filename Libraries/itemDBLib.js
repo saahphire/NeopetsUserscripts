1ca3cede57dda46431a91b6f19a19b6f0e55759d
Saah <saahloss@gmail.com>
2026-06-05 18:04:36 -0300

    Add content-type to headers

--- Libraries/itemDBLib.js
+++ Libraries/itemDBLib.js
@@ -64,11 +64,11 @@ const onUnauthorized = (scriptName, url, body) => {
 }
 
 const fetchItemDb = (url, scriptName, body) =>  new Promise((res, rej) => {
-    fetch(url, {credentials: 'include', method: body ? 'POST' : 'GET', body})
+    fetch(url, {credentials: 'include', method: body ? 'POST' : 'GET', body, headers: { 'Content-Type': 'application/json' })
         .then(response => {
             if(response.status === 200) return response.json();
             if(response.status === 401) onUnauthorized(scriptName, url, body);
-            else throw new Error(response.status);
+            else throw new Error(response.status, response.json().error);
         })
         .then(json => res(json))
         .catch(e => console.error(e));
