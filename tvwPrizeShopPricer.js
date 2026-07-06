da61256c3e629f2923681e459f5708e32b329524
Saah <saahloss@gmail.com>
2026-07-06 00:50:56 -0300

    Update SemVer

--- tvwPrizeShopPricer.js
+++ tvwPrizeShopPricer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Neopets: TVW Prize Shop Pricer
 // @namespace    https://github.com/saahphire/NeopetsUserscripts
-// @version      1.0.2
+// @version      1.0.3
 // @description  Uses itemDB's database to estimate the neopoint value (and NP per point) of each prize
 // @author       saahphire
 // @homepageURL  https://github.com/saahphire/NeopetsUserscripts
